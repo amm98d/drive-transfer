e0eb07c64531409a4e36a6e14d892f04f0201bff
Ahmed Mustafa Malik <ahmedmustafamalik98@gmail.com>
2023-07-20 17:49:06 +0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,10 +1,11 @@
 module.exports = {
   GOOGLE_DOC_TYPE: "application/vnd.google-apps.document",
-  WORD_DOC_TYPE:
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
   GOOGLE_SHEET_TYPE: "application/vnd.google-apps.spreadsheet",
-  EXCEL_SHEET_TYPE:
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  GOOGLE_SLIDE_TYPE: "application/vnd.google-apps.presentation",
   DRIVE_FOLDER_TYPE: "application/vnd.google-apps.folder",
   DRIVE_SHORTCUT_TYPE: "application/vnd.google-apps.shortcut",
+  WORD_DOC_TYPE:
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  EXCEL_SHEET_TYPE:
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 };
