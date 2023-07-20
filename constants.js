da20e170defdb734969aca8dfdec917c65a6e544
Ahmed Mustafa Malik <ahmedmustafamalik98@gmail.com>
2023-07-21 00:47:29 +0500

    closing

--- constants.js
+++ constants.js
@@ -4,8 +4,11 @@ module.exports = {
   GOOGLE_SLIDE_TYPE: "application/vnd.google-apps.presentation",
   DRIVE_FOLDER_TYPE: "application/vnd.google-apps.folder",
   DRIVE_SHORTCUT_TYPE: "application/vnd.google-apps.shortcut",
+
   WORD_DOC_TYPE:
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
   EXCEL_SHEET_TYPE:
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  POWERPOINT_TYPE:
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
 };
